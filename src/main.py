ca9ad2c29ff467b1d308a98ebd3899a2b392e3fb
Svenn-Arne Dragly <s@dragly.com>
2010-12-28 17:55:07 +0100

    Testing branc

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from math import pi, sin, cos
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from direct.task import Task
+from direct.actor.Actor import Actor
+
+#dette er en test
 
 class MyApp(ShowBase):
     def __init__(self):
@@ -14,6 +18,18 @@ class MyApp(ShowBase):
         # Apply scale and position transforms on the model.
         self.environ.setScale(0.25, 0.25, 0.25)
         self.environ.setPos(-8, 42, 0)
+        
+        # set up panda
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+
+        # set up camera and mouse settings
+        #self.disableMouse()
+        self.accept('mouse1',self.printHello)
+    def printHello(self):
+        print self.camera.getPos()
 
 app = MyApp()
 app.run()
