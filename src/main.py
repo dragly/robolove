cb9611845529417e293f8a6ed4a488e6048a2476
Svenn-Arne Dragly <s@dragly.com>
2010-12-29 14:40:09 +0100

    Minor changes

--- src/main.py
+++ src/main.py
@@ -10,27 +10,18 @@ from panda3d.core import AmbientLight,DirectionalLight
 from panda3d.core import AntialiasAttrib
 from panda3d.core import TextNode,Point3,Vec4
 from panda3d.core import loadPrcFile
+from direct.interval.IntervalGlobal import Sequence
 import sys
-#dette er en test
 
 loadPrcFile('robolove.prc')
 
 class MainApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        
-        # Check video card capabilities for shaders.
-        if (base.win.getGsg().getSupportsBasicShaders() == 0):
-            addTitle("Glow Filter: Video driver reports that shaders are not supported.")
-            return
 
-        # Load filters
-        self.filters = CommonFilters(base.win, base.cam)
-        filterok = self.filters.setBloom(blend=(0,0,0,1), desat=-0.5, intensity=3.0, size="small")
-        if (filterok == False):
-            addTitle("Toon Shader: Video card not powerful enough to do image postprocessing")
+        # Load shaders. If this fails, quit.
+        if(not loadShaders()):
             return
-        self.glowSize=1
 
         # Load the environment model.
         self.environ = self.loader.loadModel("models/environment")
@@ -60,6 +51,29 @@ class MainApp(ShowBase):
         self.pandaActor.setScale(0.3, 0.3, 0.3)
         self.pandaActor.reparentTo(self.render)
 
+        # Create the four lerp intervals needed for the panda to
+        # walk back and forth.
+        pandaPosInterval1 = self.pandaActor.posInterval(13,
+                                                        Point3(0, -10, 0),
+                                                        startPos=Point3(0, 10, 0))
+        pandaPosInterval2 = self.pandaActor.posInterval(13,
+                                                        Point3(0, 10, 0),
+                                                        startPos=Point3(0, -10, 0))
+        pandaHprInterval1 = self.pandaActor.hprInterval(3,
+                                                        Point3(180, 0, 0),
+                                                        startHpr=Point3(0, 0, 0))
+        pandaHprInterval2 = self.pandaActor.hprInterval(3,
+                                                        Point3(0, 0, 0),
+                                                        startHpr=Point3(180, 0, 0))
+
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(pandaPosInterval1,
+                                  pandaHprInterval1,
+                                  pandaPosInterval2,
+                                  pandaHprInterval2,
+                                  name="pandaPace")
+        self.pandaPace.loop()
+
         # set up camera and mouse settings
         self.camera.setPos(6.27662, -48.9656, 26.0119)
         self.camera.setHpr(7.30458, -27.7855, 3.34447)
@@ -71,19 +85,30 @@ class MainApp(ShowBase):
         self.accept("space", self.toggleGlow)
         self.accept("escape", sys.exit, [0])
         self.accept('d', self.toggleDebugMode)
+        self.accept('w', self.moveForward)
 
         # add tasks
         self.taskMgr.add(self.checkLogic, "CheckLogic")
+
+    def checkLogic(self, task):
+        # this method is a placeholder to test if differnt stuff has occured
+        # like checking wether the robot is ready for a new command, etc.
+        return Task.cont
+
+    def moveForward(self):
+        self.pandaPace.pause()
+        print self.pandaActor.getPos()
 
     def toggleGlow(self):
         self.glowSize = self.glowSize + 1
         if (self.glowSize == 4): self.glowSize = 0
         self.filters.setBloom(blend=(0,0,0,1), desat=-0.5, intensity=3.0, size=self.glowSize)
         print "Glow size set to", self.glowSize
-        
+
     def printCameraPosition(self):
         print self.camera.getPos()
         print self.camera.getHpr()
+        
     def toggleDebugMode(self):
         if(self.debugMode):
             self.setDebugMode(False)
@@ -98,12 +123,20 @@ class MainApp(ShowBase):
             self.disableMouse()
             self.ignore('c')
 
-    def checkLogic(self, task):
-        # this method is a placeholder to test if differnt stuff has occured
-        # like checking if two things are in certain positions, etc.
-        # most of this could probably by placed in collision detection,
-        # but this function is here in case we need it
-        return Task.cont
+    def loadShaders(self):
+        # Check video card capabilities for shaders.
+        if (base.win.getGsg().getSupportsBasicShaders() == 0):
+            addTitle("Glow Filter: Video driver reports that shaders are not supported.")
+            return False
+
+        # Load filters
+        self.filters = CommonFilters(base.win, base.cam)
+        filterok = self.filters.setBloom(blend=(0,0,0,1), desat=-0.5, intensity=3.0, size="small")
+        if (filterok == False):
+            addTitle("Toon Shader: Video card not powerful enough to do image postprocessing")
+            return False
+        self.glowSize=1
+        return True
 
 app = MainApp()
 app.run()
